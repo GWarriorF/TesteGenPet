1a451a06e4afa84f0b398da696eb04dc4223f0cf
rita_@Almeida <rita_aja92@hotmail.com>
2023-06-09 16:40:02 -0300

    @RitaAlmeidah Metodos e Abstract

--- src/cadastro/MetodosRitaGato.java
+++ src/cadastro/MetodosRitaGato.java
@@ -1,8 +1,6 @@
 package cadastro;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import procedimentos.VacinasAbstract;
 import cadastro.modelo.CadastroAnimalGato;
 
 public class MetodosRitaGato extends CadastroAnimalGato {
@@ -10,7 +8,7 @@ public class MetodosRitaGato extends CadastroAnimalGato {
 	public MetodosRitaGato(String nomeAnimal, String nomeDono, String cor, String sexo, String especie, String endereco,
 			int idade, String cpf, float peso, int pelo, String raca) {
 		super(nomeAnimal, nomeDono, cor, sexo, especie, endereco, idade, cpf, peso, pelo, raca);
-		// TODO Auto-generated constructor stub
+		
 	}
 	
 	float valor, valorTotal;
@@ -32,20 +30,44 @@ public class MetodosRitaGato extends CadastroAnimalGato {
 			valor = 50;
 			valorTotal += valor;
 		}
+		else if(getPelo() == 4)
+		{
+			valor = 80;
+			valorTotal += valor;
+		}
+		else
+		{
+			System.out.println("Tipo de Pelo inválido!");
+		}
 	}
 	
-    
-	/*public MetodosRitaGato(float banhoGatoPeloCurto, float banhoGatoPeloLongo, float tosaGatoMaquina,
-			float tosaGatoTesoura, float vacinarGatoV5, float vacinarGatoV3, float vacinarGatoRaiva) 
+	public void tosaGato()
 	{
-		this.banhoGatoPeloCurto = banhoGatoPeloCurto;
-		this.banhoGatoPeloLongo = banhoGatoPeloLongo;
-		this.tosaGatoMaquina = tosaGatoMaquina;
-		this.tosaGatoTesoura = tosaGatoTesoura;
-		this.vacinarGatoV5 = vacinarGatoV5;
-		this.vacinarGatoV3 = vacinarGatoV3;
-		this.vacinarGatoRaiva = vacinarGatoRaiva;
-	}*/
+		if(getPelo() == 1)
+		{
+			valor = 50;
+			valorTotal += valor;
+		}
+		else if(getPelo() == 2)
+		{
+			valor = 60;
+			valorTotal += valor;
+		}
+		else if(getPelo() == 3)
+		{
+			valor = 75;
+			valorTotal += valor;
+		}
+		else
+		{
+			System.out.println("Tipo de Pelo inválido!");
+		}
+	}
+	
+	public void vacinaGato()
+	{
+		
+	}
 
-
+	
 }
