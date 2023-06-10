563f06b2c01d938f708e75bc38ad214c69e0760d
Isamiguel <goncalvesisabellamiguel@gmail.com>
2023-06-10 11:29:12 -0300

    @IsaMiguel Classe Modelo CadastroCachorro

--- src/cadastro/AnimalModelo/CadastroAnimalCachorro.java
+++ src/cadastro/AnimalModelo/CadastroAnimalCachorro.java
@@ -1,6 +1,30 @@
 package cadastro.AnimalModelo;
 
-public class CadastroAnimalCachorro 
-{
+public class CadastroAnimalCachorro extends CadastroAnimal{
+
+private int tipoPeloCachorro;
+private String racaCachorro;
+public CadastroAnimalCachorro(String nomeAnimal, String nomeDono, String cor, String sexo, String especie,
+		String endereco, int idade, String cpf, float peso, int tipoPeloCachorro, String racaCachorro) {
+	super(nomeAnimal, nomeDono, cor, sexo, especie, endereco, idade, cpf, peso);
+	this.tipoPeloCachorro = tipoPeloCachorro;
+	this.racaCachorro = racaCachorro;
+}
+public int getTipoPeloCachorro() {
+	return tipoPeloCachorro;
+}
+public void setTipoPeloCachorro(int tipoPeloCachorro) {
+	this.tipoPeloCachorro = tipoPeloCachorro;
+}
+public String getRacaCachorro() {
+	return racaCachorro;
+}
+public void setRacaCachorro(String racaCachorro) {
+	this.racaCachorro = racaCachorro;
+}
+
 
 }
+
+
+
